1021efafda4d93bf7a1d62c2a136646d0d50605f
Parrot Live user <user@localhost.localdomain>
2020-08-09 08:20:54 +0000

    complete

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function action(val)
 {
-    console.log(val)
+  
     document.getElementById('inp').value+=val
     
 }
 function swipe()
 {
     document.getElementById("inp").value=""
+}
+function result()
+{
+    var text= document.getElementById('inp').value
+    var final = eval(text)
+    document.getElementById('inp').value=final
 }
